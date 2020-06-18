e1d04ee8d00d1f79e6ef8cfcbab668d6fe5dd88a
Louis Maxwell <louismaxwell@knights.ucf.edu>
2020-06-17 22:26:07 -0400

    files have been updated with basic outline for Covid APP

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getRandomJoke(){
+    fetch(`https://api.covid19api.com/summary`)
+    .then(response => response.json())
+    .then(responsejson => placeStat(responsejson.Global))
+    .catch(error => alert(error))
+}
 
+function placeStat(stat){
+    console.log(Object.keys(stat));
+    console.log(Object.values(stat));
+    console.log(Object.entries(stat)[0][0]);
+    let newConf = Object.entries(stat)[0][0];
+    let newConfnum = Object.entries(stat)[0][1];
+    
+    $('#covid-div').replaceWith(`<strong>${newConf} cases:</strong> ${newConfnum}`);
+}
+
+
+
+
+
+
+
+getRandomJoke()
