e106360a7369fb70218eaf81556ce85dceeee19c
Louis Maxwell <louismaxwell@knights.ucf.edu>
2020-06-22 23:14:30 -0400

    need to figure out how to use the user location to get results

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const userLocation = $('#user_current_location');
-
+const user_key = "AIzaSyDSG6JxsJRLwUirDGdkGlnHMEHcmbpvvuA";
+var selectedUserLocation;
 
 function getCovidGlobalStats(){
     fetch(`https://api.covid19api.com/summary`)
     .then(response => response.json())
     .then(responsejson => placeStat(responsejson.Global))
     .catch(error => alert(error))
+}
+
+function getLocationSpecificCovidStats(country){
+    console.log(country);
+    fetch(`https://api.covid19api.com/summary`)
+    .then(response => response.json())
+    // Figure out how to get the users (country) to match the object for the data
+    .then(responsejson => console.log(responsejson));
+    
 }
 
 function placeStat(stat){
@@ -22,6 +32,8 @@ function placeStat(stat){
     let totalDeathsNum = Object.entries(stat)[3][1];
     let totalRecovered = Object.entries(stat)[5][0];
     let totalRecoveredNum= Object.entries(stat)[5][1];
+    // to get yesterdays deaths, subtract new deaths from today, from total deaths
+
     
     // $('#covid-div').replaceWith(`<strong>${newConf} cases:</strong> ${newConfNum}`);
     $('#covid-div').append(`<div><strong>${newConf} cases:</strong> ${newConfNum}</div>`);
@@ -35,11 +47,30 @@ function getUserLocation(){
     console.log(navigator.geolocation.getCurrentPosition(showCoordinate));
 }
 
+function setUserLocation(latitude,longitude){
+    fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${user_key}`)
+    .then(response => response.json())
+    .then(responsejson => {
+        // console.log(responsejson.results[9].address_components[0].long_name);
+        selectedUserLocation = responsejson.results[9].address_components[0].long_name;
+        console.log(selectedUserLocation);
+        // getLocationSpecificCovidStats(selectedUserLocation);
+        if(selectedUserLocation.includes(' ')){
+            let replacedStr = selectedUserLocation.split(' ').join('-');
+            console.log(replacedStr);
+            getLocationSpecificCovidStats(replacedStr);
+        }
+        
+    });
+}
+
+
 function showCoordinate(position){
     let long = position.coords.longitude;
     let lati = position.coords.latitude;
     console.log(long);
     console.log(lati);
+    setUserLocation(lati,long);
 }
 
 userLocation.on("click", (e) => {
