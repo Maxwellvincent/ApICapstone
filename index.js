5df8b03acf377461c739884377965d24ab10c389
Louis Maxwell <louismaxwell@knights.ucf.edu>
2020-06-24 14:39:56 -0400

    Got user location stats to generate dynamically onto page

--- index.js
+++ index.js
@@ -14,8 +14,48 @@ function getLocationSpecificCovidStats(country){
     fetch(`https://api.covid19api.com/summary`)
     .then(response => response.json())
     // Figure out how to get the users (country) to match the object for the data
-    .then(responsejson => console.log(responsejson));
+    .then(responsejson => 
+        {
+            // Generates an array of objects containing countries
+            let listOfCountries = responsejson.Countries;
+            // console.log(listOfCountries.find((data) => console.log(data.Slug)));
+
+
+            listOfCountries.filter((data) => {
+                    if(data.Slug == country) {
+                        console.log(data);
+                        placeUserspecificLocationStats(data);
+                    };
+                })
+
+
+
+            // console.log(listOfCountries.filter((data) => {
+            //     if(data.Slug == country) {
+            //         console.log(data)
+                    
+            //     };
+            // }));
+        })
+    .catch((error) => alert(error));
     
+}
+
+function placeUserspecificLocationStats(data){
+    // data here is in reference to an object, that contains properties as Current Data, NewConfirmed cases, NewDeaths, NewRecovered, Slug, TotalConfirmed, TotalDeaths, TotalRecovered
+        let userTotalCases = data.TotalConfirmed;
+        let currentDate = data.Date;
+        let userNewCases = data.NewConfirmed;
+        let userDeathsToday = data.NewDeaths;
+        let userCurrentLocation = data.Country;
+        console.log(userTotalCases);
+        $('#user-current-location').append(` ${userCurrentLocation}`);
+        $('#current-date').append(` ${currentDate}`);
+        $('#user-total-cases').append(` ${userTotalCases}`);
+        $('#user-new-cases').append(` ${userNewCases}`);
+        $('#user-new-deaths-today').append(` ${userDeathsToday}`);
+
+        // could possibly loop through this data and place all stats.
 }
 
 function placeStat(stat){
@@ -43,8 +83,18 @@ function placeStat(stat){
     $('#covid-div').append(`<div><strong>${totalRecovered} cases:</strong> ${totalRecoveredNum}</div>`);
 }
 
+// Grabs the user's location from browser, user has to allow browser to grab their location.
 function getUserLocation(){
     console.log(navigator.geolocation.getCurrentPosition(showCoordinate));
+}
+
+// This functions handles the position data the is generated within getCurrentPosition, and grabs long, and latt of the user and sets it to setUserLocation funciton
+function showCoordinate(position){
+    let long = position.coords.longitude;
+    let lati = position.coords.latitude;
+    console.log(long);
+    console.log(lati);
+    setUserLocation(lati,long);
 }
 
 function setUserLocation(latitude,longitude){
@@ -57,23 +107,17 @@ function setUserLocation(latitude,longitude){
         // getLocationSpecificCovidStats(selectedUserLocation);
         if(selectedUserLocation.includes(' ')){
             let replacedStr = selectedUserLocation.split(' ').join('-');
-            console.log(replacedStr);
-            getLocationSpecificCovidStats(replacedStr);
+            console.log(replacedStr.toLowerCase());
+            getLocationSpecificCovidStats(replacedStr.toLowerCase());
         }
         
     });
 }
 
 
-function showCoordinate(position){
-    let long = position.coords.longitude;
-    let lati = position.coords.latitude;
-    console.log(long);
-    console.log(lati);
-    setUserLocation(lati,long);
-}
-
 userLocation.on("click", (e) => {
+    // Need to clear information if clicked again!!!!
+    // function clearData()
     // what do we want to happen when this is clicked?
     // We want to grab the user's current location
     console.log("this was clicked");
